9be6bce7edfccc7054c568123a56ad352ec4a0aa
cabitibaly <bitibalyclementalex@gmail.com>
2025-10-08 14:23:57 +0000

    hero section description projet => done

---
+++ src/app/projets/[id]/page.tsx
@@ -0,0 +1,15 @@
+"use client"
+
+import Navbar from "@/components/navigation/navbar"
+import HeroDescription from "@/components/projet/description/heroDescription"
+
+const DescriptionProjet = () => {
+    return (
+        <>        
+            <Navbar/>
+            <HeroDescription/>
+        </>
+    )
+}
+
+export default DescriptionProjet
