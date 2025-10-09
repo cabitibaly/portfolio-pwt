83351be0b5078d8e9c7bfc7c008cd8558449ca78
cabitibaly <bitibalyclementalex@gmail.com>
2025-10-09 08:22:54 +0000

    hero section a-propos => done

---
+++ src/app/a-propos/page.tsx
@@ -0,0 +1,14 @@
+"use client"
+import HeroApropos from '@/components/a-propos/heroApropos'
+import Navbar from '@/components/navigation/navbar'
+
+const Apropos = () => {
+    return (
+        <>
+            <Navbar />
+            <HeroApropos />
+        </>
+    )
+}
+
+export default Apropos
