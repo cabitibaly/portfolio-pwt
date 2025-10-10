70ff6b0c557b5abc0f817eb8ee444d4030e311c5
cabitibaly <bitibalyclementalex@gmail.com>
2025-10-10 09:10:46 +0000

    Amélioration de l'expérience de scroll avec la position sticky

--- src/components/a-propos/nosValeursSection.tsx
+++ src/components/a-propos/nosValeursSection.tsx
@@ -6,16 +6,18 @@ import CallToAction from "../cards/callToAction"
 
 const NosValeurSection = () => {
     return (
-        <section className="overflow-hidden relative top-0 bg-bleu-2 w-screen h-screen flex items-center justify-center">
-            <div className="sticky top-0 z-0 w-full h-full flex items-center justify-center">
-                <div className="absolute w-full h-full flex items-center justify-center">
-                    <div className="animate-blob absolute left-1/12 translate-x-1/2 w-1/3 aspect-square rounded-full bg-bleu-9 filter blur-[20px] max-lg:w-2/5 max-lg:-left-[6%] max-md:translate-none max-md:left-0 max-md:w-2/3"></div>
-                    <div className="animate-blob delay-300 absolute right-1/12 -translate-x-1/2 w-1/3 aspect-square rounded-full bg-vert-9 filter blur-[20px] max-lg:w-2/5 max-lg:-right-[6%] max-md:translate-none max-md:right-0 max-md:w-2/3"></div>
-                    <div className="animate-blob delay-800 absolute bottom-[6%] w-1/3 aspect-square rounded-full bg-jaune filter blur-[20px] max-md:bottom-[10%] max-lg:w-2/5 max-md:w-2/3 max-xs:bottom-1/5"></div>
-                </div>                
-            </div>
-            <div className="overflow-x-hidden px-[100px] py-16 absolute z-30 top-0 left-0 w-full h-full flex flex-col items-center justify-start gap-12 max-896:px-4 max-896:py-8">                
+        <>
+            <section className="overflow-hidden sticky top-0 py-16 bg-bleu-2 w-screen h-screen flex flex-col items-center justify-start">
                 <h2 className="text-5xl text-gris-12 text-center font-medium">Nos valeurs</h2>
+                <div className="relative top-0 z-0 w-full h-full flex items-center justify-center">
+                    <div className="absolute w-full h-full flex items-center justify-center">
+                        <div className="animate-blob absolute left-1/12 translate-x-1/2 w-1/3 aspect-square rounded-full bg-bleu-9 filter blur-[20px] max-lg:w-2/5 max-lg:-left-[6%] max-md:translate-none max-md:left-0 max-md:w-2/3"></div>
+                        <div className="animate-blob delay-300 absolute right-1/12 -translate-x-1/2 w-1/3 aspect-square rounded-full bg-vert-9 filter blur-[20px] max-lg:w-2/5 max-lg:-right-[6%] max-md:translate-none max-md:right-0 max-md:w-2/3"></div>
+                        <div className="animate-blob delay-800 absolute bottom-[6%] w-1/3 aspect-square rounded-full bg-jaune filter blur-[20px] max-md:bottom-[10%] max-lg:w-2/5 max-md:w-2/3 max-xs:bottom-1/5"></div>
+                    </div>                
+                </div>            
+            </section>
+            <div className="overflow-hidden px-[100px] py-16 relative z-30 top-0 left-0 w-screen min-h-screen flex flex-col items-center justify-start gap-12 max-896:px-4 max-896:py-8">                                
                 <div className="w-full flex flex-col items-center justify-start gap-10">
                     <div className="w-full flex items-center justify-between gap-8 max-lg:flex-col">
                         <InfoCard 
@@ -43,7 +45,7 @@ const NosValeurSection = () => {
                 </div>
                 <CallToAction />
             </div>
-        </section>
+        </>
     )
 }
 
--- src/components/projet/description/gallerieProjet.tsx
+++ src/components/projet/description/gallerieProjet.tsx
@@ -4,10 +4,14 @@ import Image from "next/image"
 
 const GallerieProjet = () => {
     return (
-        <section className="overflow-hidden gallerie-bg relative top-0 bg-bleu-2 w-screen h-screen flex items-center justify-center">
-            <div className="sticky top-0 left-0 z-20 border border-bleu-1/40 p-6 bg-gris-8/10 filter backdrop-blur-sm w-screen h-screen"></div>
-            <div className="overflow-x-hidden px-[100px] py-16 absolute z-30 top-0 left-0 w-full h-full flex flex-col items-center justify-start gap-12 max-896:px-4 max-896:py-8">                
-                <h2 className="text-5xl text-gris-1 text-center font-medium">Galerie</h2>
+        <>
+            <section className="overflow-hidden gallerie-bg sticky top-0 bg-bleu-2 w-screen h-screen flex flex-col items-center justify-start">                
+                <div className="sticky top-0 left-0 z-10 border border-bleu-1/40 p-6 bg-gris-8/10 filter backdrop-blur-sm w-screen h-screen"></div>            
+                <div className="absolute left-1/2 top-16 z-20 -translate-x-1/2 -translate-y-1/2 w-full flex items-center justify-center">
+                    <h2 className="text-5xl text-gris-1 text-center font-medium">Galerie</h2>
+                </div>
+            </section>
+            <div className="px-[100px] py-16 relative z-30 top-0 left-0 w-full min-h-screen flex flex-col items-center justify-start gap-12 max-896:px-4 max-896:py-8">                                
                 <div className="w-full flex flex-col items-center justify-start gap-10">
                     <div className="w-full flex items-center justify-between gap-8 max-lg:flex-col max-md:gap-4">
                         <div className="relative w-full aspect-square rounded-2xl ">
@@ -22,7 +26,7 @@ const GallerieProjet = () => {
                     </div>
                 </div>
             </div>
-        </section>
+        </>
     )
 }
 
